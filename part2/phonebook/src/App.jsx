f3301657a0ffd6e3fa67f84db0c165f634897d33
AA <30997769+kerosene110@users.noreply.github.com>
2026-09-05 17:34:09 +0000

    Ex 14-15 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react'
 import noteService from './services/notes'
 
-const Filter = ({personsToShow, handleFilterChange}) => {
+const Filter = ({ personsToShow, handleFilterChange }) => {
   return (
     <div>
-      filter shown with <input value={personsToShow} onChange={handleFilterChange}/>
+      filter shown with <input value={personsToShow} onChange={handleFilterChange} />
     </div>
   )
 }
@@ -23,9 +23,19 @@ const PersonForm = ({ handleFormSubmit, newName, handleNameChange, newNum, handl
   </form>)
 }
 
-const Persons = ({ persons, personsToShow }) => {
-  const matchedPersons = persons.filter(person => person.name.toLowerCase().includes(personsToShow.toLowerCase()))
-  return matchedPersons.map(person => <p key={person.name}>{person.name} {person.number}</p>)
+const Persons = ({ persons, personsToShow, setPersons }) => {
+  let matchedPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(personsToShow.toLowerCase())
+  )
+  const handleDelete = (name, id) => {
+    if (window.confirm(`Delete ${name}?`)) {
+      noteService.deletePerson(id)
+      setPersons(matchedPersons.filter(person => person.id !== id))
+    }
+  }
+  return matchedPersons.map(person =>
+    <p key={person.name}>{person.name} {person.number} <button onClick={() => handleDelete(person.name, person.id)}>delete</button></p>
+  )
 }
 
 const App = () => {
@@ -57,7 +67,12 @@ const App = () => {
   const handleFormSubmit = (event) => {
     event.preventDefault()
     if (persons.map(p => p.name).indexOf(newName) !== -1) {
-      alert(`${newName} is already added to phonebook`)
+      if (window.confirm(`${newName} is already added to phonebook. Replace the old number with a new one?`)) {
+        const personObject = { ...persons.find(p => p.name === newName), number: newNum }
+        noteService.update(personObject.id, personObject).then(returnedObj => {
+          setPersons(persons.map(p => p.id !== personObject.id ? p : returnedObj))
+        })
+      }
       return
     }
 
@@ -66,7 +81,7 @@ const App = () => {
       number: newNum,
       id: persons.length + 1
     }
-    
+
     noteService.create(personObject).then(returnedObj => {
       setPersons(persons.concat(returnedObj))
     })
@@ -78,13 +93,13 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter personsToShow={personsToShow} handleFilterChange={handleFilterChange}/>
+      <Filter personsToShow={personsToShow} handleFilterChange={handleFilterChange} />
 
       <h3>Add a new</h3>
       <PersonForm handleFormSubmit={handleFormSubmit} newName={newName} handleNameChange={handleNameChange} newNum={newNum} handleNumChange={handleNumChange} />
 
       <h3>Numbers</h3>
-      <Persons persons={persons} personsToShow={personsToShow}/>
+      <Persons persons={persons} personsToShow={personsToShow} setPersons={setPersons} />
 
     </div>
   )
--- part2/phonebook/src/services/notes.js
+++ part2/phonebook/src/services/notes.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 const baseUrl = 'http://localhost:3001/persons'
 
 const getAll = () => {
-    return axios.get(baseUrl)
-        .then(response => response.data)
+  return axios.get(baseUrl)
+    .then(response => response.data)
 }
 
 const create = (newObject) => {
@@ -16,4 +16,9 @@ const update = (id, newObject) => {
   return request.then(response => response.data)
 }
 
-export default { getAll, create, update }
+const deletePerson = (id) => {
+  const request = axios.delete(`${baseUrl}/${id}`)
+  return request.then(response => response.data)
+}
+
+export default { getAll, create, update, deletePerson }
