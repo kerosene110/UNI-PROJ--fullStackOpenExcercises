8d1bc7447feb8c23ed8afb62700cb77bbbd5ee23
AA <30997769+kerosene110@users.noreply.github.com>
2026-09-16 14:51:03 +0000

    Ex 3.15-3.18 done

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -9,19 +9,33 @@ app.use(express.json())
 app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post_data'))
 
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(results => {
-    response.json(results)
-  })
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+    .then(results => {
+      response.json(results)
+    })
+    .catch(error => next(error))
 })
 
-app.get('/info', (request, response) => {
-  Person.find({}).then(results => {
-    response.send(
-      `<p>Phonebook has info for ${results.length} people</p>
-      <p>${new Date()}</p>`
-    )
-  })
+app.get('/info', (request, response, next) => {
+  Person.find({})
+    .then(results => {
+      response.send(
+        `<p>Phonebook has info for ${results.length} people</p>
+        <p>${new Date()}</p>`
+      )
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
@@ -48,17 +62,18 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name || !body.number) {
     return response.status(400).json({ error: 'name or number missing' })
   }
 
-  Person.find({ name: body.name }).then(result => {
-    if (result.length > 0) {
-      return response.status(409).json({ error: 'name must be unique' })
-    }
+  Person.find({ name: body.name })
+    .then(result => {
+      if (result.length > 0) {
+        return response.status(409).json({ error: 'name must be unique' })
+      }
 
     const entry = new Person({
       name: body.name,
@@ -66,7 +81,34 @@ app.post('/api/persons', (request, response) => {
     })
     entry.save().then(response.json(entry))
   })
+    .catch(error => next(error))
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  if (!body.number) {
+    return response.status(400).json({ error: 'number missing' })
+  }
+
+  Person.findById(request.params.id)
+    .then(p => {
+      if (!p) {
+        return response.status(404).end()
+      }
+
+      p.name = body.name
+      p.number = body.number
+
+      return p.save().then((updatedPerson) => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch((error) => next(error))
+})
+
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
