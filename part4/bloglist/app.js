fb3a6a71e9212aa27de576e0e4293dd89af6d0b3
AA <30997769+kerosene110@users.noreply.github.com>
2026-09-19 13:57:56 +0000

    Ex 4.2 done

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,35 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
-const Blog = require('./models/blog')
-// const blogsRouter = require('./controllers/blogs')
+const blogsRouter = require('./controllers/blogs')
 
 const app = express()
 
 mongoose.connect(config.MONGODB_URL, { family: 4 })
-// mongoose
-//   .connect(config.MONGODB_URL)
-//   .then(() => {
-//     console.info('connected to MongoDB')
-//   })
-//   .catch((error) => {
-//     console.error('error connection to MongoDB:', error.message)
-//   })
 
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
+console.log(blogsRouter)
+app.use('/api/blogs', blogsRouter)
 module.exports = app
