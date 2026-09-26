7ed0edae21d7540e53c8706de21fcf51129b11b9
AA <30997769+kerosene110@users.noreply.github.com>
2026-09-26 16:40:06 +0000

    Ex 4.8 done

--- part4/bloglist/tests/blogs_api.test.js
+++ part4/bloglist/tests/blogs_api.test.js
@@ -3,10 +3,29 @@ const { test, after, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const helper = require('./data')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
 
 beforeEach(async () => {
-  await Note.deleteMany({})
-  await Note.insertMany(helper.initialNotes)
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.blogs)
+})
+
+// Ex 4.8
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('there are two blogs', async () => {
+  const response = await api.get('/api/blogs')
+  assert.strictEqual(response.body.length, helper.blogs.length)
+})
+
+after(async () => {
+  await mongoose.connection.close()
 })
